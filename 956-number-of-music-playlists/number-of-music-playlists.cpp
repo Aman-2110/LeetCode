c07613be14394463a1148c0a6f44bbb0eef8c168
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-11 03:11:47 +0530

    Time: 8 ms (17.47%) | Memory: 8 MB (48.90%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.cpp
+++ 956-number-of-music-playlists/number-of-music-playlists.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int numMusicPlaylists(int n, int goal, int k) {
         int mod = 1e9 + 7;
-        vector<int> curr(n + 1, 0), prev(n + 1, 0);
-        prev[0] = 1;
+        vector dp(goal + 1, vector<int> (n + 1, 0));
+        dp[0][0] = 1;
         for(int i = 1 ; i <= goal ; i++){
             for(int j = 1 ; j <= n ; j++){
-                curr[j] = ((n - (j - 1ll))*prev[j - 1])%mod;
-
                 if(j > k)
-                    curr[j] = (curr[j] + (j - k - 0ll)*prev[j])%mod;
+                    dp[i][j] = (dp[i][j] + (j - k - 0ll)*dp[i - 1][j])%mod;
+                
+                dp[i][j] = (dp[i][j] + (n - (j - 1ll))*dp[i - 1][j - 1])%mod;
             }
-            prev = curr;
         }
 
-        return prev[n];
+        return dp[goal][n];
     }
 };
