e0223f3a78be1ef0bbd448418c17a1c5f9275774
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-11 03:11:17 +0530

    Time: 5 ms (34.52%) | Memory: 6.5 MB (68.76%) - LeetSync

--- 956-number-of-music-playlists/number-of-music-playlists.cpp
+++ 956-number-of-music-playlists/number-of-music-playlists.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int helper(int currGoal, long uniq, int k, int n, int goal, vector<vector<int>> &dp){
-        if(currGoal == goal){
-            if(uniq == n)
-                return 1;
-            else
-                return 0;
+    int numMusicPlaylists(int n, int goal, int k) {
+        int mod = 1e9 + 7;
+        vector<int> curr(n + 1, 0), prev(n + 1, 0);
+        prev[0] = 1;
+        for(int i = 1 ; i <= goal ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                curr[j] = ((n - (j - 1ll))*prev[j - 1])%mod;
+
+                if(j > k)
+                    curr[j] = (curr[j] + (j - k - 0ll)*prev[j])%mod;
+            }
+            prev = curr;
         }
 
-        if(dp[currGoal][uniq] != -1) 
-            return dp[currGoal][uniq];
-
-        long count = 0;
-
-        if(uniq != n)
-            count = (count + (n - uniq)*helper(currGoal + 1, uniq + 1, k, n, goal, dp))%mod;
-        
-        if(uniq > k)
-            count = (count + (uniq - k)*helper(currGoal + 1, uniq, k, n, goal, dp))%mod;
-
-        return dp[currGoal][uniq] = count;
-    }
-    int numMusicPlaylists(int n, int goal, int k) {
-        vector dp(goal + 1, vector<int> (n + 1, -1));
-        return helper(0, 0, k, n, goal, dp);
+        return prev[n];
     }
 };
