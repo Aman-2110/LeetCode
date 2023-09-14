7bf12563ad800459220032e0f2d4f68a47920925
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-14 14:41:57 +0530

    Time: 16 ms (85.13%) | Memory: 15.8 MB (19.44%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void eulerPath(string &curr, vector<string> &res, unordered_map<string, vector<string>> &adj){
+    void eulerPath(string &curr, vector<string> &res, unordered_map<string, priority_queue<string, vector<string>, greater<string>>> &adj){
         while(adj[curr].size() != 0){
-            string back = adj[curr].back();
-            adj[curr].pop_back();
+            string back = adj[curr].top();
+            adj[curr].pop();
             eulerPath(back, res, adj);
         }
         res.push_back(curr);
     }
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-        sort(tickets.begin(), tickets.end());
-        reverse(tickets.begin(), tickets.end());
-        unordered_map<string, vector<string>> adj;
-        for(auto edge : tickets)
-            adj[edge[0]].push_back(edge[1]);
 
-        
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj;
+        for(auto edge : tickets)
+            adj[edge[0]].push(edge[1]);
+
         vector<string> res;
         string startPoint = "JFK";
         eulerPath(startPoint, res, adj);
