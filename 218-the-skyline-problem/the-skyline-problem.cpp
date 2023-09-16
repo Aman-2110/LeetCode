278f3ec5f8ffac9e826c07a64e20ada4b1a7b8af
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-16 15:17:22 +0530

    Time: 190 ms (5.80%) | Memory: 50.6 MB (5.03%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.cpp
+++ 218-the-skyline-problem/the-skyline-problem.cpp
@@ -33,19 +33,30 @@ public:
         
         sort(buildings.begin(), buildings.end(), second<vector<int>>);
         que = priority_queue<tuple<int, int, int>>();
-        
+        vector<vector<int>> res2;
+
         for(int i = 0 ; i < n ; i++){
             while(!que.empty() && get<1>(que.top()) > buildings[i][1])
                 que.pop();
             
             if(que.empty() || get<0>(que.top()) < buildings[i][2])
-                res.push_back({buildings[i][1], que.empty() ? 0 : get<0>(que.top())});
+                res2.push_back({buildings[i][1], que.empty() ? 0 : get<0>(que.top())});
             
             que.push({buildings[i][2], buildings[i][0], buildings[i][1]});
         }
 
-        sort(res.begin(), res.end());
+        int i = 0, j = res2.size() - 1;
+        vector<vector<int>> res3;
+        while(i != res.size() || j != -1){
+            if(i == res.size())
+                res3.push_back(res2[j--]);
+            else if(j == -1 || res[i][0] < res2[j][0])
+                res3.push_back(res[i++]);
+            else if(res[i][0] > res2[j][0])
+                res3.push_back(res2[j--]);
+        }
 
-        return res;
+
+        return res3;
     }
 };
