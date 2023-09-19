ad91213294f67526de44cfaf57046c5ee61ca913
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-19 19:46:55 +0530

    Time: 138 ms (23.11%) | Memory: 84.3 MB (14.31%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int i : nums){
+            if(st.find(i) != st.end())
+                return i;
+            st.insert(i);
+        }
+        return -1;
+    }
+};
