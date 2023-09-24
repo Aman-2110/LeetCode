e474be32113a785dc9c51abbea8bde15c4b3faca
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-25 01:31:39 +0530

    Time: 10 ms (45.16%) | Memory: 17.6 MB (32.11%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+public:
+
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> pyramid(query_row + 1, vector<double>(101, 0.0));
+
+        pyramid[0][0] = poured;
+        for (int row = 0; row < query_row; row++) {
+            for (int glass = 0; glass <= row; glass++) {
+                double overflow = (pyramid[row][glass] - 1.0) / 2.0;
+
+                if (overflow > 0) {
+                    pyramid[row + 1][glass] += overflow;
+                    pyramid[row + 1][glass + 1] += overflow;
+                }
+            }
+        }
+        return min(1.0, pyramid[query_row][query_glass]);
+    }
+};
+       
