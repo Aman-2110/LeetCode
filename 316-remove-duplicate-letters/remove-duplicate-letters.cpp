b6d317c45ce27276c1b10cf80cad97cdbca0c872
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-09-26 15:33:39 +0530

    Time: 2 ms (59.76%) | Memory: 7.2 MB (6.80%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-   stack<char> charStack;
-
-    // Frequency array
-    vector<int> count(26, 0); 
-
-    // Boolean array to track whether a character is in the stack
-     vector<bool> inStack(26, false);
-    
-    // Count the occurrences of each character in the string
-    for (char c : s) {
-        count[c - 'a']++;  }
-
-     // Decrement the count of the current character
-    for (char c : s) {
-        count[c - 'a']--;
+        int n = s.size();
+        unordered_map<char, int> mp;
+        for(int i = 0 ; i < n ; i++)
+            mp[s[i]] = i;
         
-        // If the character is already in the stack, skip it    
-        if (inStack[c - 'a']) {
-                continue; 
-                          }
-            
-        // Pop characters from the stack as long as the current character is smaller
-        // and there are more occurrences of the character at the top of the stack
-        while (!charStack.empty() && c < charStack.top() && count[charStack.top() - 'a'] > 0) {
-                inStack[charStack.top() - 'a'] = false;
-                charStack.pop();
+        stack<char> st;
+        vector<bool> vis(26, false);
+
+        for(int i = 0 ; i < n ; i++){
+            if(vis[s[i] - 'a'])
+                continue;
+
+            while(!st.empty() && st.top() > s[i] && mp[st.top()] >= i){
+                vis[st.top() - 'a'] = false;
+                st.pop();
             }
-            
-        // Push the current character onto the stack
-            charStack.push(c);
-            inStack[c - 'a'] = true;
-                      }
-    
-    // Construct the result string by popping characters from the stack
-    string result = "";
-    while (!charStack.empty()) {
-        result = charStack.top() + result;
-        charStack.pop();
+
+            vis[s[i] - 'a'] = true;
+            st.push(s[i]);
+        }
+
+        string res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
     }
-    
-    return result;
-   }
 };
