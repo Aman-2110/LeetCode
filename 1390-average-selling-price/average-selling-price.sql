6ed0c1487bbed0f37d1efaf04d290f8ffe8467cb
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-10-16 18:13:00 +0530

    Time: 1330 ms (82.79%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,9 @@
-select p.product_id, CASE WHEN (ROUND(SUM(CAST(p.price * u.units as Float))/SUM(u.units), 2)) IS NULL THEN 0 ELSE ROUND(SUM(CAST(p.price * u.units as Float))/SUM(u.units), 2) END as "average_price"
-from Prices p left join UnitsSold u
-on u.product_id = p.product_id and u.purchase_date >= p.start_date and u.purchase_date <= end_date
-group by p.product_id
+select p.product_id, (
+  case
+    when count(p.product_id) = 1 then if(u.units is null,0,p.price)
+    else round(sum(p.price * u.units) / sum(u.units),2) # aggregate function by default ignore null values
+  end
+) as average_price
+from prices as p left join unitssold as u
+on p.product_id = u.product_id and (u.purchase_date between p.start_date and p.end_date)
+group by p.product_id;
