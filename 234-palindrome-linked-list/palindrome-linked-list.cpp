ebfd3fb606c5d2fc1502a44d7be71d85fa1a7f5b
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-10-17 18:08:11 +0530

    Time: 206 ms (15.29%) | Memory: 123 MB (33.01%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,39 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* rev(ListNode *head, ListNode *newHead) {
-        if(head == NULL)
-            return newHead;
+    ListNode *left;
+    bool isPalindrome(ListNode* right) {
+        if(!left)
+            left = right;
 
-        ListNode *nxt = head->next;
-        head->next = newHead;
-    
-        return rev(nxt, head);
-    }
-
-    bool isPalindrome(ListNode* head) {
-        if(!head->next)
+        if(!right)
             return true;
 
-        ListNode *slow = head, *fast = head->next;
-
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
+        if(isPalindrome(right->next)){
+            if(right->val == left->val){
+                left = left->next;
+                return true;
+            }
         }
-
-        ListNode *ptr = rev(slow->next, NULL), *curr = head;
         
-        while(ptr){
-            if(ptr->val != curr->val)
-                return false;
-            ptr = ptr->next;
-            curr = curr->next;
-        }
-
-        return true;
+        return false; 
     }
 };
