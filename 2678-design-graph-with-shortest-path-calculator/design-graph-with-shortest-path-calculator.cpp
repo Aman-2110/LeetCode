7e17b910dfb650f9666fba2c9f663e523f52f93b
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-11 15:35:11 +0530

    Time: 143 ms (91.88%) | Memory: 76.1 MB (73.95%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -0,0 +1,41 @@
+class Graph {
+public:
+    vector<vector<pair<int, int>>> adj;
+    Graph(int n, vector<vector<int>>& edges) {
+        adj.resize(n);
+        for(auto &v : edges)
+            adj[v[0]].push_back({v[1], v[2]});
+    }
+    
+    void addEdge(vector<int> edge) {
+        adj[edge[0]].push_back({edge[1], edge[2]});
+    }
+    
+    int shortestPath(int node1, int node2) {
+        vector<int> dist(adj.size(), INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+        que.push({0, node1});
+        dist[node1] = 0;
+        while(!que.empty()){
+            auto [a, b] = que.top();
+            que.pop();
+            if(b == node2)
+                return a;
+
+            for(auto [neigh, cost] : adj[b]){
+                if(dist[neigh] > a + cost){
+                    que.push({a + cost, neigh});
+                    dist[neigh] = a + cost;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph* obj = new Graph(n, edges);
+ * obj->addEdge(edge);
+ * int param_2 = obj->shortestPath(node1,node2);
+ */
