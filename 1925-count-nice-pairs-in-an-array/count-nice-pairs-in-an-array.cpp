d78cec4da1f1e5e08f9b1b5e32cbfa85b700aac6
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-21 15:44:08 +0530

    Time: 91 ms (75.14%) | Memory: 57 MB (96.91%) - LeetSync

---
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rev(int num){
+        int res = 0;
+        while(num != 0){
+            res *= 10;
+            res += (num % 10);
+            num /= 10;
+        }
+        return res;
+    }
+
+    int countNicePairs(vector<int>& nums) {
+        long res = 0, mod = 1e9 + 7, n = nums.size();
+        unordered_map<int, int> mp;
+        for(int i = 0 ; i < n ; i++){
+            int num = nums[i] - rev(nums[i]);
+            res = (res + mp[num]++)%mod;
+        }
+        return res;
+    }
+};
