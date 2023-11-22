d7b95d85556db0b7e12a12f9cdf2aefd3b894c8c
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-22 12:56:20 +0530

    Time: 147 ms (82.26%) | Memory: 89.5 MB (67.09%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -3,18 +3,20 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         cin.tie(0)->sync_with_stdio(0);
         cout.tie(0)->sync_with_stdio(0);
-        map<int, vector<int>> mp;
+        vector<vector<int>> mp;
         for(int i = 0 ; i < nums.size() ; i++){
             for(int j = 0 ; j < nums[i].size() ; j++){
-                mp[i + j].push_back(nums[i][j]);
+                if(i + j == mp.size())
+                    mp.push_back({nums[i][j]});
+                else
+                    mp[i + j].push_back(nums[i][j]);
             }
         }
 
         vector<int> res;
-        for(auto p : mp){
-            while(p.second.size() != 0){
-                res.push_back(p.second.back());
-                p.second.pop_back();
+        for(int i = 0 ; i < mp.size() ; i++){
+            for(int j = mp[i].size() - 1 ; j >= 0 ; j--){
+                res.push_back(mp[i][j]);
             }
         }
 
