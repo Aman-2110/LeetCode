cfc85892bdd8a17de897750fee3169c0ecd4d244
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-23 16:10:26 +0530

    Time: 176 ms (5.19%) | Memory: 75.9 MB (5.84%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> res;
+        int n = l.size();
+        for(int i = 0 ; i < n ; i++){
+            int a = INT_MAX, last = INT_MIN;
+            unordered_set<int> st;
+            for(int j = l[i] ; j <= r[i] ; j++){
+                a = min(nums[j], a);
+                last = max(nums[j], last);
+                st.insert(nums[j]);
+            }
+            
+            bool flag = false;
+            if((last - a) % (r[i] - l[i]) == 0){
+                int d = (last - a) / (r[i] - l[i]);
+                
+                while(a != last){
+                    if(st.find(a) == st.end())
+                        break;
+                    a += d;
+                }
+            }
+
+            res.push_back(a == last);
+        }
+
+        return res;
+    }
+};
