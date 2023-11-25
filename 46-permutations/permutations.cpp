a978a060ad6a3468913f6e3ae589029cda020d7e
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-25 23:43:38 +0530

    Time: 2 ms (63.44%) | Memory: 8.3 MB (32.51%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    set<vector<int>> st;
+    void helper(vector<int> &vec, map<int, int> &mp, int n){
+        if(vec.size() == n){
+            st.insert(vec);
+        }
+
+        for(auto p : mp){
+            if(mp[p.first] > 0){
+                vec.push_back(p.first);
+                mp[p.first]--;
+                helper(vec, mp, n);
+                vec.pop_back();
+                mp[p.first]++;
+            }
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        map<int, int> mp;
+        for(int i : nums)
+            mp[i]++;
+        vector<int> vec;
+        helper(vec, mp, nums.size());
+        vector<vector<int>> res(st.begin(), st.end());
+        return res; 
+    }
+};
