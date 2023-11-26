99d3d8d02e02f2aecae30baac672d1e21f103bba
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-11-26 14:12:36 +0530

    Time: 119 ms (99.43%) | Memory: 66.1 MB (96.02%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
+        cin.tie(0)->sync_with_stdio(false);
+        cout.tie(0)->sync_with_stdio(false);
         int n = matrix.size(), m = matrix[0].size();
         for(int i = 0 ; i < m ; i++){
             for(int j = 1 ; j < n ; j++){
@@ -14,37 +16,9 @@ public:
 
         for(int i = 0 ; i < n ; i++){
             sort(matrix[i].begin(), matrix[i].end(), greater<int>());
-            for(int j = 0 ; j < m ; j++){
+            for(int j = 0 ; j < m ; j++)
                 res = max((j + 1)*matrix[i][j], res);
-            }
         }
         return res;
     }
 };
-
-
-
-// 1
-// 3
-// 2
-// 1 0 1 0
-// 1 1 0 0
-
-
-
-// 0 0 1
-// 1 1 2
-// 2 0 3
-
-
-// 1 0 0
-// 2 1 1
-// 3 2 0
-
-// 1 1 0
-// 2 1 0 
-
-
-
-// 1 1 0
-// 1 0 1
