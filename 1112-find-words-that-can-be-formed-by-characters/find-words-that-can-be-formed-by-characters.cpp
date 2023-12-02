ebed296203a1f33eae28bac7bdb4aa873cd1272e
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2023-12-02 14:50:43 +0530

    Time: 22 ms (100.00%) | Memory: 15.8 MB (87.44%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -3,12 +3,12 @@ public:
     int countCharacters(vector<string>& words, string chars) {
         cin.tie(0)->sync_with_stdio(0);
         int res = 0;
-        vector<int> vec(26, 0);
+        int vec[26] = {0};
         for(char c : chars)
             vec[c - 'a']++;
 
         for(int i = 0 ; i < words.size() ; i++){
-            vector<int> temp(26, 0);
+            int temp[26] = {0};
             bool flag = true;
             for(int j = 0 ; j < words[i].size() ; j++){
                 temp[words[i][j] - 'a']++;
