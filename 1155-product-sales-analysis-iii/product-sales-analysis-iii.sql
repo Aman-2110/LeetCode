15c785487e09a3d57b05c01dd22cddb9f137195c
Aman-2110 <77570873+Aman-2110@users.noreply.github.com>
2024-05-11 14:47:12 +0530

    Time: 2704 ms (10.45%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,7 @@
-SELECT A.product_id, A.year as first_year, A.quantity, A.price
-FROM Sales A INNER JOIN (
+SELECT product_id, year as first_year, quantity, price
+FROM Sales NATURAL JOIN (
     SELECT product_id, min(year) as year
     FROM Sales
     GROUP BY product_id
 ) B
-ON A.product_id = B.product_id AND A.year = B.year
+-- ON A.product_id = B.product_id AND A.year = B.year
